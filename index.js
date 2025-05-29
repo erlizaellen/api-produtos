d92527f0c1109ed3d96056e5a9b661bfe6ee8671
erlizaellen <erlizaellen@gmail.com>
2025-05-29 11:58:31 -0300

    api com try/catch

--- index.js
+++ index.js
@@ -1,53 +1,65 @@
 const express = require('express')
 const app = express()
 const port = 6579
-const produtosDb = []
-const usersDb =[]
+const {Pool}= require('pg')
+
+const pool= new Pool ({
+        user:'postgres.japnckvirjblabaypvop',
+        host:'aws-0-sa-east-1.pooler.supabase.com',
+        database:'postgres',
+        password:'Geracao2025',
+        port:5432
+    })
+
+
 
 app.use(express.json())
 
-app.get("/" ,(req,res)=> {
-    res.send("olá erlizaellen !")
-})
-
-
-
-app.post("/produtos" , (req , res)=> {
-    const {nome,preco,categoria} = req.body
-
+app.post('/produtos',async (req,res)=>{
+    const {nome,preco ,categoria ,image_url}=req.body
     
-    if(!nome || !preco || !categoria){
-        return res.status(400).send("nome, preco e categoria são obrigátorios")
+    if(!nome || !preco || !categoria || !image_url){
+        return res.status(400).send('Todos os campos são obrigatórios')
     }
 
-    const produto = {
-        nome: nome,
-        preco:preco,
-        categoria:categoria
+    if (nome.length > 100){
+        return res.status(400).send ('Nome pode ter no máximo 100 caracteres')
     }
 
-    produtosDb.push(produto)
+    if (categoria.length > 50){
+        return res.status(400).send('Categoria pode ter no máximo 50 carateres')
+    }
+   
+    try {
+        const produto = await pool.query(`
+            INSERT INTO produtos (nome,preco,categoria,image_url)
+            VALUES (
+              '${nome}',
+               ${preco},
+              '${categoria}',
+              '${image_url}'
+            )
+            RETURNING *
+       `)
     
-    res.status(201).send(produto)
+       res.status(201).send(produto.rows[0])
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('Erro ao cadastrar produto')
+    }
+} )   
 
-})
-
-app.get("/produtos", (req , res) => {
-    res.send(produtosDb)
-})
-
-app.post("/usuarios", (req ,res) => {
-    const {nome,email,senha} = req.body
-
-    if(!nome || !email || !senha){
-        return res.status(400).send("email , nome e senha são obrigátorios")
+app.get('/produtos', async (req,res)=> {
+    try {
+        const produtos= await pool.query('SELECT * FROM produtos')
+        return res.status(200).send(produtos.rows)
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send('Erro ao buscar produtos')
+        
     }
 
-    usersDb.push({nome,email,senha})
-    res.status(201).send("Usuário cadastrado com sucesso!")
 })
-
-
 
 app.listen(port,()=>{
     console.log(`o servidor está rodando na porta ${port}`)
